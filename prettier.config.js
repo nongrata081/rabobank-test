03c2ddb34cddf1dcb494fddaf9b47ab3f3a23c9c
Vahan Minasian <vaanatwork@gmail.com>
2018-09-27 16:14:44 +0200

    chore($built-app): built app for gh pages deploy

--- prettier.config.js
+++ prettier.config.js
@@ -10,6 +10,5 @@ module.exports = {
 	singleQuote: true,
 	trailingComma: 'none', // other options `es5` or `all`
 	bracketSpacing: true,
-	arrowParens: 'avoid', // other option 'always'
-	parser: 'typescript'
+	arrowParens: 'avoid' // other option 'always'
 };

d61869c3e3aa5f7abfa60725457e17aa14384004
Vahan Minasian <vaanatwork@gmail.com>
2018-09-28 19:23:15 +0200

    feat($app-shell): add app shell

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule, MatIconModule } from '@angular/material';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
 	declarations: [AppComponent],
@@ -12,7 +13,8 @@ import { MatButtonModule, MatIconModule } from '@angular/material';
 		AppRoutingModule,
 		BrowserAnimationsModule,
 		MatButtonModule,
-		MatIconModule
+		MatIconModule,
+		RouterModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]

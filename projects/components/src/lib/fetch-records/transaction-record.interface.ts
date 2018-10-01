bb0417c768e7be17785b495dbe99cc4684bc8ff9
Vahan Minasian <vaanatwork@gmail.com>
2018-10-01 20:19:25 +0200

    test($fetch_records): add unit tests for invalid records service

---
+++ projects/components/src/lib/fetch-records/transaction-record.interface.ts
@@ -0,0 +1,9 @@
+export interface TransactionRecord {
+	invalidRecords: string;
+	reference: string;
+	accountNumber: string;
+	description: string;
+	startBalance: string;
+	mutation: string;
+	endBalance: string;
+}

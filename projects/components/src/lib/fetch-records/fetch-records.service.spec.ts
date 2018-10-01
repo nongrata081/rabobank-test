865958617fcdb2d62692678f2c5a32cebdaa8356
Vahan Minasian <vaanatwork@gmail.com>
2018-10-01 20:19:25 +0200

    test($fetch_records): add unit tests for invalid records service

--- projects/components/src/lib/fetch-records/fetch-records.service.spec.ts
+++ projects/components/src/lib/fetch-records/fetch-records.service.spec.ts
@@ -1,18 +1,54 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 
 import { FetchRecordsService } from './fetch-records.service';
+import {
+	HttpClientTestingModule,
+	HttpTestingController
+} from '@angular/common/http/testing';
+// import { TransactionRecords } from './transaction-record.interface';
 
-describe('FetchRecordsService', () => {
+fdescribe('FetchRecordsService', () => {
+	let service: FetchRecordsService;
+	let httpMock: HttpTestingController;
+
 	beforeEach(() => {
 		TestBed.configureTestingModule({
+			imports: [HttpClientTestingModule],
 			providers: [FetchRecordsService]
 		});
+
+		service = TestBed.get(FetchRecordsService);
+		httpMock = TestBed.get(HttpTestingController);
 	});
 
-	it('should be created', inject(
-		[FetchRecordsService],
-		(service: FetchRecordsService) => {
-			expect(service).toBeTruthy();
-		}
-	));
+	afterEach(() => {
+		httpMock.verify();
+	});
+
+	fit('should retrieve records from json file', () => {
+		const transactionRecordsMock = [
+			{
+				reference: '112806',
+				accountNumber: 'NL74ABNA0248990274',
+				description: 'Candy from Jan de Vries',
+				startBalance: '109.75',
+				mutation: '-23.94',
+				endBalance: '85.81'
+			},
+			{
+				reference: '112806',
+				accountNumber: 'NL91RABO0315273637',
+				description: 'Clothes from Richard Theu�',
+				startBalance: '31.03',
+				mutation: '+0.27',
+				endBalance: '31.3'
+			}
+		];
+		service.getJSON().subscribe(records => {
+			expect(records.length).toBe(2);
+		});
+		const request = httpMock.expectOne(service.url);
+		expect(request.request.method).toBe('GET');
+		request.flush(transactionRecordsMock);
+	});
 });
--- projects/components/src/lib/fetch-records/fetch-records.service.ts
+++ projects/components/src/lib/fetch-records/fetch-records.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { TransactionRecord } from './transaction-record.interface';
 
 @Injectable({
 	providedIn: 'root'
 })
 export class FetchRecordsService {
+	url = './records/dist/invalid-records.json';
 	constructor(private http: HttpClient) {}
 
 	public getJSON(): Observable<any> {
-		return this.http.get('./records/dist/invalid-records.json');
+		return this.http.get<any>(this.url);
 	}
 }

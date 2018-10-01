61bdd79588709d734f380a4534625341e8a44ef2
Vahan Minasian <vaanatwork@gmail.com>
2018-10-01 18:52:46 +0200

    feat($invalid_recs_report): add components, service for report

--- projects/components/src/lib/components.module.ts
+++ projects/components/src/lib/components.module.ts
@@ -1,9 +1,20 @@
 import { NgModule } from '@angular/core';
-import { ComponentsComponent } from './components.component';
+import { HeaderComponent } from './header/header.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { FooterComponent } from './footer/footer.component';
+import { InvalidRecordsComponent } from './invalid-records/invalid-records.component';
+import { MatTableModule } from '@angular/material';
+import { FetchRecordsService } from './fetch-records/fetch-records.service';
 
 @NgModule({
-	imports: [],
-	declarations: [ComponentsComponent],
-	exports: [ComponentsComponent]
+	imports: [MDBBootstrapModule.forRoot(), MatTableModule],
+	declarations: [HeaderComponent, FooterComponent, InvalidRecordsComponent],
+	exports: [
+		HeaderComponent,
+		FooterComponent,
+		InvalidRecordsComponent,
+		MatTableModule
+	],
+	providers: [FetchRecordsService]
 })
 export class ComponentsModule {}

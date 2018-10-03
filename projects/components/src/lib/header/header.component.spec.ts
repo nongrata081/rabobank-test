d1db341a5be8add6392fbf60b1a120aa323065d3
Vahan Minasian <vaanatwork@gmail.com>
2018-10-03 14:55:48 +0200

    test($add_unit_tests): add unit tests for lib

--- projects/components/src/lib/header/header.component.spec.ts
+++ projects/components/src/lib/header/header.component.spec.ts
@@ -1,11 +1,18 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+	async,
+	ComponentFixture,
+	TestBed
+} from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
 describe('HeaderComponent', () => {
 	let component: HeaderComponent;
 	let fixture: ComponentFixture<HeaderComponent>;
+	let de: DebugElement;
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
@@ -17,10 +24,27 @@ describe('HeaderComponent', () => {
 	beforeEach(() => {
 		fixture = TestBed.createComponent(HeaderComponent);
 		component = fixture.componentInstance;
+		de = fixture.debugElement;
 		fixture.detectChanges();
 	});
 
 	it('should create', () => {
 		expect(component).toBeTruthy();
+	});
+
+	it('should have a "Customer statement processor" title', () => {
+		expect(component.title).toEqual(
+			'Customer statement processor'
+		);
+	});
+
+	it('should render text in navbar', () => {
+		expect(
+			de.query(
+				By.css(
+					'mdb-navbar mdb-navbar-brand span.navbar-brand'
+				)
+			).nativeElement.innerText
+		).toContain('Customer statement processor');
 	});
 });
--- projects/components/src/lib/invalid-records/invalid-records.component.spec.ts
+++ projects/components/src/lib/invalid-records/invalid-records.component.spec.ts
@@ -1,27 +1,58 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+	async,
+	ComponentFixture,
+	fakeAsync,
+	TestBed
+} from '@angular/core/testing';
 
 import { InvalidRecordsComponent } from './invalid-records.component';
 import { MatTableModule } from '@angular/material';
 import { HttpClientModule } from '@angular/common/http';
+import { DebugElement } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { FetchRecordsService } from '../fetch-records/fetch-records.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { By } from '@angular/platform-browser';
 
 describe('InvalidRecordsComponent', () => {
 	let component: InvalidRecordsComponent;
 	let fixture: ComponentFixture<InvalidRecordsComponent>;
+	let de: DebugElement;
+	let fetchRecordsService: FetchRecordsService;
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
-			imports: [MatTableModule, HttpClientModule],
-			declarations: [InvalidRecordsComponent]
+			imports: [MatTableModule, HttpClientTestingModule],
+			declarations: [InvalidRecordsComponent],
+			providers: [FetchRecordsService]
 		}).compileComponents();
+
+		fetchRecordsService = TestBed.get(FetchRecordsService);
 	}));
 
 	beforeEach(() => {
-		fixture = TestBed.createComponent(InvalidRecordsComponent);
+		fixture = TestBed.createComponent(
+			InvalidRecordsComponent
+		);
 		component = fixture.componentInstance;
+		de = fixture.debugElement;
 		fixture.detectChanges();
 	});
 
 	it('should create', () => {
 		expect(component).toBeTruthy();
+	});
+
+	it('should have "reference" and "describe" columns for data table', () => {
+		expect(component.displayedColumns).toEqual([
+			'reference',
+			'description'
+		]);
+	});
+
+	it('should have records data defined', () => {
+		fetchRecordsService.getJSON().subscribe(data => {
+			expect(data).toBeDefined();
+		});
 	});
 });
